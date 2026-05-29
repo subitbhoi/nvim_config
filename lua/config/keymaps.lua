18f0e57431fd75a3f430d70f45ae317c1b3c28af
Subit Bhoi <bhoisubit@gmail.com>
2026-05-29 18:20:09 +0530

    snacks: keymaps for picker configured
    keymaps for picker and telescope both are there but the telescope
    keymaps are commented out for future usability

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,7 @@ local map = vim.keymap.set
 local icons = require("core.icons")
 
 local function desc(text)
-  return { noremap = true, silent = true, desc = text }
+	return { noremap = true, silent = true, desc = text }
 end
 
 local general = icons.general
@@ -58,14 +58,45 @@ map("n", "<leader>bl", "<cmd>ls<cr>", desc(nav.buffer .. " List buffers"))
 -----------------------------------------------------------
 -- Search
 -----------------------------------------------------------
+--telescope
 
-map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", desc(general.find .. " Find files"))
-map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", desc(general.search .. " Live grep"))
-map("n", "<leader>fb", "<cmd>Telescope buffers<cr>", desc(nav.buffer .. " Buffers"))
-map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", desc(general.help .. " Help tags"))
-map("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc(general.recent .. " Recent files"))
-map("n", "<leader>fc", "<cmd>Telescope commands<cr>", desc(general.command .. " Commands"))
-map("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", desc(general.keymap .. " Keymaps"))
+-- map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", desc(general.find .. " Find files"))
+-- map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", desc(general.search .. " Live grep"))
+-- map("n", "<leader>fb", "<cmd>Telescope buffers<cr>", desc(nav.buffer .. " Buffers"))
+-- map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", desc(general.help .. " Help tags"))
+-- map("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc(general.recent .. " Recent files"))
+-- map("n", "<leader>fc", "<cmd>Telescope commands<cr>", desc(general.command .. " Commands"))
+-- map("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", desc(general.keymap .. " Keymaps"))
+
+-- Snacks picker
+
+map("n", "<leader>ff", function()
+	Snacks.picker.files()
+end, { desc = "Find Files" })
+map("n", "<leader>fw", function()
+	Snacks.picker.grep()
+end, { desc = "Live Grep (Words)" })
+map("n", "<leader>fb", function()
+	Snacks.picker.buffers()
+end, { desc = "Find Buffers" })
+map("n", "<leader>fr", function()
+	Snacks.picker.recent()
+end, { desc = "Recent Files" })
+map("n", "<leader>fc", function()
+	Snacks.picker.colorschemes()
+end, { desc = "Colorschemes" })
+map("n", "<leader>fC", function()
+	Snacks.picker.commands()
+end, { desc = "Commands" })
+map("n", "<leader>fh", function()
+	Snacks.picker.help()
+end, { desc = "Help Pages" })
+map("n", "<leader>fk", function()
+	Snacks.picker.keymaps()
+end, { desc = "Keymaps" })
+map("n", "<leader>sR", function()
+	Snacks.picker.resume()
+end, { desc = "Resume Last Search" })
 
 -----------------------------------------------------------
 -- File tree / navigation
@@ -78,8 +109,12 @@ map("n", "<leader>o", "<cmd>Neotree focus<cr>", desc(nav.explorer .. " Focus exp
 -- Diagnostics / LSP
 -----------------------------------------------------------
 
-map("n", "[d", function() vim.diagnostic.jump({ count = -1 }) end, desc(diag.diagnostic .. " Previous diagnostic"))
-map("n", "]d", function() vim.diagnostic.jump({ count = 1 }) end, desc(diag.diagnostic .. " Next diagnostic"))
+map("n", "[d", function()
+	vim.diagnostic.jump({ count = -1 })
+end, desc(diag.diagnostic .. " Previous diagnostic"))
+map("n", "]d", function()
+	vim.diagnostic.jump({ count = 1 })
+end, desc(diag.diagnostic .. " Next diagnostic"))
 map("n", "<leader>ud", vim.diagnostic.open_float, desc(diag.diagnostic .. " Show diagnostics"))
 
 map("n", "K", vim.lsp.buf.hover, desc(lsp.hover .. " Hover"))
@@ -90,7 +125,7 @@ map("n", "gr", vim.lsp.buf.references, desc(lsp.references .. " References"))
 map("n", "<leader>rn", vim.lsp.buf.rename, desc(lsp.rename .. " Rename"))
 map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, desc(lsp.code_action .. " Code action"))
 map("n", "<leader>lf", function()
-  vim.lsp.buf.format({ async = true })
+	vim.lsp.buf.format({ async = true })
 end, desc(lsp.formatting .. " Format"))
 
 -----------------------------------------------------------
@@ -98,78 +133,148 @@ end, desc(lsp.formatting .. " Format"))
 -----------------------------------------------------------
 
 local function with_gitsigns(fn)
-  return function(...)
-    local ok, gs = pcall(require, "gitsigns")
-    if not ok then
-      return
-    end
-    return fn(gs, ...)
-  end
+	return function(...)
+		local ok, gs = pcall(require, "gitsigns")
+		if not ok then
+			return
+		end
+		return fn(gs, ...)
+	end
 end
 
-map("n", "]c", with_gitsigns(function(gs)
-  if vim.wo.diff then
-    vim.cmd.normal({ "]c", bang = true })
-  else
-    gs.nav_hunk("next")
-  end
-end), desc(git.change .. " Next hunk"))
+map(
+	"n",
+	"]c",
+	with_gitsigns(function(gs)
+		if vim.wo.diff then
+			vim.cmd.normal({ "]c", bang = true })
+		else
+			gs.nav_hunk("next")
+		end
+	end),
+	desc(git.change .. " Next hunk")
+)
 
-map("n", "[c", with_gitsigns(function(gs)
-  if vim.wo.diff then
-    vim.cmd.normal({ "[c", bang = true })
-  else
-    gs.nav_hunk("prev")
-  end
-end), desc(git.change .. " Previous hunk"))
+map(
+	"n",
+	"[c",
+	with_gitsigns(function(gs)
+		if vim.wo.diff then
+			vim.cmd.normal({ "[c", bang = true })
+		else
+			gs.nav_hunk("prev")
+		end
+	end),
+	desc(git.change .. " Previous hunk")
+)
 
-map("n", "<leader>gs", with_gitsigns(function(gs)
-  gs.stage_hunk()
-end), desc(git.add .. " Stage hunk"))
+map(
+	"n",
+	"<leader>gs",
+	with_gitsigns(function(gs)
+		gs.stage_hunk()
+	end),
+	desc(git.add .. " Stage hunk")
+)
 
-map("n", "<leader>gr", with_gitsigns(function(gs)
-  gs.reset_hunk()
-end), desc(git.delete .. " Reset hunk"))
+map(
+	"n",
+	"<leader>gr",
+	with_gitsigns(function(gs)
+		gs.reset_hunk()
+	end),
+	desc(git.delete .. " Reset hunk")
+)
 
-map("v", "<leader>gs", with_gitsigns(function(gs)
-  gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-end), desc(git.add .. " Stage selection"))
+map(
+	"v",
+	"<leader>gs",
+	with_gitsigns(function(gs)
+		gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+	end),
+	desc(git.add .. " Stage selection")
+)
 
-map("v", "<leader>gr", with_gitsigns(function(gs)
-  gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-end), desc(git.delete .. " Reset selection"))
+map(
+	"v",
+	"<leader>gr",
+	with_gitsigns(function(gs)
+		gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+	end),
+	desc(git.delete .. " Reset selection")
+)
 
-map("n", "<leader>gS", with_gitsigns(function(gs)
-  gs.stage_buffer()
-end), desc(git.staged .. " Stage buffer"))
+map(
+	"n",
+	"<leader>gS",
+	with_gitsigns(function(gs)
+		gs.stage_buffer()
+	end),
+	desc(git.staged .. " Stage buffer")
+)
 
-map("n", "<leader>gR", with_gitsigns(function(gs)
-  gs.reset_buffer()
-end), desc(git.delete .. " Reset buffer"))
+map(
+	"n",
+	"<leader>gR",
+	with_gitsigns(function(gs)
+		gs.reset_buffer()
+	end),
+	desc(git.delete .. " Reset buffer")
+)
 
-map("n", "<leader>gp", with_gitsigns(function(gs)
-  gs.preview_hunk()
-end), desc(git.change .. " Preview hunk"))
+map(
+	"n",
+	"<leader>gp",
+	with_gitsigns(function(gs)
+		gs.preview_hunk()
+	end),
+	desc(git.change .. " Preview hunk")
+)
 
-map("n", "<leader>gb", with_gitsigns(function(gs)
-  gs.blame_line({ full = true })
-end), desc(git.blame .. " Blame line"))
+map(
+	"n",
+	"<leader>gb",
+	with_gitsigns(function(gs)
+		gs.blame_line({ full = true })
+	end),
+	desc(git.blame .. " Blame line")
+)
 
-map("n", "<leader>gd", with_gitsigns(function(gs)
-  gs.diffthis()
-end), desc(git.change .. " Diff this"))
+map(
+	"n",
+	"<leader>gd",
+	with_gitsigns(function(gs)
+		gs.diffthis()
+	end),
+	desc(git.change .. " Diff this")
+)
 
-map("n", "<leader>gD", with_gitsigns(function(gs)
-  gs.diffthis("~")
-end), desc(git.change .. " Diff against HEAD"))
+map(
+	"n",
+	"<leader>gD",
+	with_gitsigns(function(gs)
+		gs.diffthis("~")
+	end),
+	desc(git.change .. " Diff against HEAD")
+)
 
-map("n", "<leader>gt", with_gitsigns(function(gs)
-  gs.toggle_current_line_blame()
-end), desc(git.blame .. " Toggle blame"))
+map(
+	"n",
+	"<leader>gt",
+	with_gitsigns(function(gs)
+		gs.toggle_current_line_blame()
+	end),
+	desc(git.blame .. " Toggle blame")
+)
 
-map("n", "<leader>gw", with_gitsigns(function(gs)
-  gs.toggle_word_diff()
-end), desc(git.change .. " Toggle word diff"))
+map(
+	"n",
+	"<leader>gw",
+	with_gitsigns(function(gs)
+		gs.toggle_word_diff()
+	end),
+	desc(git.change .. " Toggle word diff")
+)
 
 -----------------------------------------------------------
 -- UI / toggles
